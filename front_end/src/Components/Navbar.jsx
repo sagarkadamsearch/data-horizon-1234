a06e4acd6567fa9b23c91c2c8333ab324f57ed00
ashwini2704 <ashwini.titarmare96@gmail.com>
2023-12-14 22:57:23 +0530

    day-2

--- front_end/src/Components/Navbar.jsx
+++ front_end/src/Components/Navbar.jsx
@@ -8,7 +8,6 @@ export const Navbar = () => {
            <div>Logo</div>
            <div>
               <Link to={"/"}>Home</Link>
-              <Link to={"/dashboard"}>Dashboard</Link>
               <Link to={"/login"}>Login</Link>
            </div>
         </NAV>
